859b6548448b45e82b068303e9594f60228a07bc
Muammir <muammir45@gmail.com>
2020-10-18 23:08:02 +0700

    my second commit

--- userinput.c
+++ userinput.c
@@ -3,7 +3,7 @@ int main(void){
 /*This is my second program in C*/
 int age;
 printf("Hi, how old are you?");
-printf("%d",&age);
+scanf("%d",&age);
 printf("You are %d years old",age);
 return(0);
 }
